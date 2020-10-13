05aaa573c45771b3dfd0a44383700240f584685f
catalinlup <minecraft.lupau@gmail.com>
2020-10-13 11:52:35 +0200

    add basic bvh with median split

--- src/bounding_volume_hierarchy.cpp
+++ src/bounding_volume_hierarchy.cpp
@@ -1,11 +1,93 @@
 #include "bounding_volume_hierarchy.h"
 #include "draw.h"
 
+unsigned long long Node::id = 0;
+unsigned long long BvhObject::id = 0;
+
 BoundingVolumeHierarchy::BoundingVolumeHierarchy(Scene* pScene)
     : m_pScene(pScene)
 {
 
     // as an example of how to iterate over all meshes in the scene, look at the intersect method below
+
+    // load the data from the scene
+    addBvhObjectsFromScene();
+
+    // BVH Tree Creation
+
+    // get all the bvhObjectIds
+    std::vector<unsigned long long> bvhObjectIds = getAllMapKeys(bvhObjects);
+
+
+    // create the root node at level 0
+    _root_id = createNode(bvhObjectIds, 0);
+
+
+    // create a queue for a BFS traversal of the tree
+    std::queue<std::pair<unsigned long long, unsigned int>> node_level_queue;
+
+    // insert the node _root_id and it's level inside the queue
+    node_level_queue.push(std::make_pair(_root_id, 0));
+
+    // do a BFS traversal of the tree
+    while(node_level_queue.size() > 0) {
+        std::pair<unsigned long long, unsigned int> node_level = node_level_queue.front();
+        node_level_queue.pop();
+        
+        unsigned long long node_id = node_level.first;
+        unsigned int level = node_level.second;
+        Node& node = nodes[node_id];
+
+        std::cout << level << std::endl;
+
+        _max_level_achieved = std::max(_max_level_achieved, level);
+
+
+
+        // if the node is a leaf, we should stop splitting
+        if(node.isLeaf())
+            continue;
+        
+        // otherwise, split the objects of the node into 2 groups
+        std::vector<unsigned long long> objectsAtNode = objects_at_node[node_id];
+        std::array<std::vector<unsigned long long>, 2> first_second = medianSplit(objectsAtNode, level % 3);
+
+        // create 2 new nodes based on the split, and add them as children to the current node as well as in the queue, for further processing
+        unsigned long long node_left_id = createNode(first_second[0], level + 1);
+        unsigned long long node_right_id = createNode(first_second[1], level + 1);
+
+        node.addChild(nodes[node_left_id]);
+        node.addChild(nodes[node_right_id]);
+
+        // add them to the queue
+        node_level_queue.push(std::make_pair(node_left_id, level + 1));
+        node_level_queue.push(std::make_pair(node_right_id, level + 1));
+    }
+}
+
+unsigned long long BoundingVolumeHierarchy::createNode(std::vector<unsigned long long>& bvhObjectIds, unsigned int level) {
+
+    bool isLeaf = (bvhObjectIds.size() <= 1 || _max_level == level) ? true : false;
+
+    std::vector<BvhObject> objects = getMapValuesOfKeys(bvhObjects, bvhObjectIds);
+    Node n = Node(objects, isLeaf);
+
+    // if the node is a leaf node, add references to the objects
+    if(isLeaf) {
+        for(int i = 0; i < objects.size(); i++) {
+            n.addChild(objects[i]);
+        }
+    }
+
+    // add node to the nodes map
+    nodes.insert(std::make_pair(n.getId(), n));
+    // add node to nodes at level map
+    nodes_at_level[level].push_back(n.getId());
+    // asscociate the objects to the node
+    objects_at_node.insert(std::make_pair(n.getId(), bvhObjectIds));
+
+    return n.getId();
+
 }
 
 // Use this function to visualize your BVH. This can be useful for debugging. Use the functions in
@@ -14,19 +96,106 @@ BoundingVolumeHierarchy::BoundingVolumeHierarchy(Scene* pScene)
 void BoundingVolumeHierarchy::debugDraw(int level)
 {
 
-    // Draw the AABB as a transparent green box.
-    //AxisAlignedBox aabb{ glm::vec3(-0.05f), glm::vec3(0.05f, 1.05f, 1.05f) };
-    //drawShape(aabb, DrawMode::Filled, glm::vec3(0.0f, 1.0f, 0.0f), 0.2f);
+    // // Draw the AABB as a transparent green box.
+    // //AxisAlignedBox aabb{ glm::vec3(-0.05f), glm::vec3(0.05f, 1.05f, 1.05f) };
+    // //drawShape(aabb, DrawMode::Filled, glm::vec3(0.0f, 1.0f, 0.0f), 0.2f);
 
-    // Draw the AABB as a (white) wireframe box.
-    AxisAlignedBox aabb { glm::vec3(-0.05f), glm::vec3(0.05f, 1.05f, 1.05f) };
-    //drawAABB(aabb, DrawMode::Wireframe);
-    drawAABB(aabb, DrawMode::Filled, glm::vec3(0.05f, 1.0f, 0.05f), 0.1);
+    // // Draw the AABB as a (white) wireframe box.
+    // AxisAlignedBox aabb { glm::vec3(-0.05f), glm::vec3(0.05f, 1.05f, 1.05f) };
+    // //drawAABB(aabb, DrawMode::Wireframe);
+    // drawAABB(aabb, DrawMode::Filled, glm::vec3(0.05f, 1.0f, 0.05f), 0.1);
+
+    unsigned int level_to_extrat = std::min((unsigned int)level, _max_level);
+
+    std::vector<unsigned long long> nodes_to_draw_ids = nodes_at_level[level_to_extrat];
+    std::vector<Node> nodes_to_draw = getMapValuesOfKeys(nodes, nodes_to_draw_ids);
+
+    for(int i = 0; i < nodes_to_draw.size(); i++) {
+        Node &n = nodes_to_draw[i];
+        drawAABB(n._boundingBox, DrawMode::Filled, glm::vec3(0.05f, 1.0f, 0.05f), 0.1);
+    }
+}
+
+
+// loads the data from the scene as BvhObjects
+void BoundingVolumeHierarchy::addBvhObjectsFromScene() {
+    // create BvhObjects for all elements of the scene (spheres and triangles), and add them to the bvhObjects map
+
+    // triangles
+
+    for (const auto &mesh : m_pScene->meshes)
+    {
+        for (const auto &tri : mesh.triangles)
+        {
+            glm::vec3 v0 = mesh.vertices[tri[0]].p;
+            glm::vec3 v1 = mesh.vertices[tri[1]].p;
+            glm::vec3 v2 = mesh.vertices[tri[2]].p;
+
+            BvhObject obj(v0, v1, v2);
+
+            bvhObjects.insert(std::make_pair(obj.getId(), obj));
+        }
+    }
+
+    // spheres
+
+    for (int i = 0; i < m_pScene->spheres.size(); i++)
+    {
+        Sphere sphere = m_pScene->spheres[i];
+
+        BvhObject obj = BvhObject(sphere);
+
+        bvhObjects.insert(std::make_pair(obj.getId(), obj));
+    }
+}
+
+    // performs a split of the provided objects, based on the median value
+    // split_axis = 0 - for x_axis; 1 - for y_axis; 2 - for z_axis
+std::array<std::vector<unsigned long long>, 2> BoundingVolumeHierarchy::medianSplit(std::vector<unsigned long long> bvhObjectIds, unsigned int split_axis)
+{
+    if(split_axis > 2)
+        throw std::invalid_argument("Invalid axis");
+
+    std::vector<std::pair<float, unsigned long long>> axis_values;
+
+    // extract the coordinates of the correct axis
+    for(auto id : bvhObjectIds) {
+        BvhObject obj = bvhObjects[id];
+        AxisAlignedBox aabb = obj.getAABB();
+
+        float weight = aabb.lower.x;
+
+        if(split_axis == 1)
+            weight = aabb.lower.y;
+        else
+            weight = aabb.lower.z;
+
+        axis_values.push_back(std::make_pair(weight, id));
+    }
+
+    // sort the coordinate vector
+    std::sort(axis_values.begin(), axis_values.end());
+
+    // split in 2 halves
+    std::vector<unsigned long long> firstHalf;
+    std::vector<unsigned long long> secondHalf;
+
+    for(int i = 0; i < axis_values.size() / 2; i++)
+        firstHalf.push_back(axis_values[i].second);
+    
+    for(int i = axis_values.size() / 2 ; i < axis_values.size(); i++)
+        secondHalf.push_back(axis_values[i].second);
+
+    std::array<std::vector<unsigned long long>, 2> arr;
+    arr[0] = firstHalf;
+    arr[1] = secondHalf;
+
+    return arr;
 }
 
 int BoundingVolumeHierarchy::numLevels() const
 {
-    return 5;
+    return _max_level_achieved;
 }
 
 // Return true if something is hit, returns false otherwise. Only find hits if they are closer than t stored
@@ -52,4 +221,196 @@ bool BoundingVolumeHierarchy::intersect(Ray& ray, HitInfo& hitInfo) const
     for (const auto& sphere : m_pScene->spheres)
         hit |= intersectRayWithShape(sphere, ray, hitInfo);
     return hit;
+}
+
+
+// BvhObject Implementation
+
+
+//ctr default
+BvhObject::BvhObject() {
+    // generate id foe the object
+    
+
+}
+
+
+// ctr triangle
+BvhObject::BvhObject(glm::vec3& v0, glm::vec3& v1, glm::vec3& v2) {
+
+    _id = id++;
+
+    // construct AABB for the triangle
+    _boundingBox.lower.x = std::min(std::min(v0.x, v1.x), v2.x);
+    _boundingBox.lower.y = std::min(std::min(v0.y, v1.y), v2.y);
+    _boundingBox.lower.z = std::min(std::min(v0.z, v1.z), v2.z);
+
+    _boundingBox.upper.x = std::max(std::max(v0.x, v1.x), v2.x);
+    _boundingBox.upper.y = std::max(std::max(v0.y, v1.y), v2.y);
+    _boundingBox.upper.z = std::max(std::max(v0.z, v1.z), v2.z);
+
+    // set the type of the object to triangle
+    _type = _TRIANGLE_TYPE;
+
+    // store reference to the triangle
+    _triangle[0] = v0;
+    _triangle[1] = v1;
+    _triangle[2] = v2;
+}
+
+// cre sphere
+BvhObject::BvhObject(Sphere& sphere) {
+
+    _id = id++;
+
+    // construct the AABB for the sphere
+    _boundingBox.lower.x = sphere.center.x - sphere.radius;
+    _boundingBox.lower.y = sphere.center.y - sphere.radius;
+    _boundingBox.lower.z = sphere.center.z - sphere.radius;
+
+    _boundingBox.upper.x = sphere.center.x + sphere.radius;
+    _boundingBox.upper.y = sphere.center.y + sphere.radius;
+    _boundingBox.upper.z = sphere.center.z + sphere.radius;
+
+    // set the type
+    _type = _SPHERE_TYPE;
+
+    // store reference to the sphere
+    _sphere[0] = sphere;
+}
+
+std::array<glm::vec3, 3>& BvhObject::getTriangle() {
+    if (_type != _TRIANGLE_TYPE)
+        throw std::exception(); 
+
+    return _triangle;
+}
+
+Sphere& BvhObject::getSphere() {
+    if (_type != _SPHERE_TYPE)
+        throw std::exception();
+
+    return _sphere[0];
+}
+
+AxisAlignedBox& BvhObject::getAABB() {
+    return _boundingBox;
+}
+
+std::string BvhObject::getType() {
+    return _type;
+}
+
+unsigned long long BvhObject::getId() {
+
+    return _id;
+}
+
+
+
+
+// Node Implementation
+
+
+Node::Node() {
+
+}
+
+Node::Node(std::vector<BvhObject>& containingBvhObjectss, bool isLeaf) {
+
+    // set the id
+    _id = id++;
+
+    // set the isLeaf
+
+    _isLeaf = isLeaf;
+
+    // initialize the bounding box
+    _boundingBox.lower.x = std::numeric_limits<float>::max();
+    _boundingBox.lower.y = std::numeric_limits<float>::max();
+    _boundingBox.lower.z = std::numeric_limits<float>::max();
+
+    _boundingBox.upper.x = std::numeric_limits<float>::min();
+    _boundingBox.upper.y = std::numeric_limits<float>::min();
+    _boundingBox.upper.z = std::numeric_limits<float>::min();
+
+    for (auto object : containingBvhObjectss) {
+        updateAABB(object.getAABB());
+    }
+}
+
+bool Node::isLeaf() {
+    return _isLeaf;
+}
+
+void Node::addChild(Node& other) {
+    if(isLeaf()) {
+        throw std::exception();
+    }
+
+    _children.push_back(other.getId());
+    
+}
+
+void Node::addChild(BvhObject& object) {
+    if (!isLeaf()){
+        throw std::exception();
+    }
+
+    _children.push_back(object.getId());
+}
+
+void Node::updateAABB(AxisAlignedBox& other) {
+
+    // the AABB corresponding to the node should be the smallest one that contains all other AABBs
+    // hence the minimum point should be the minimum of all the children's minimum points
+    // the maximum point should be the maximum of all the children's maximum points.
+    _boundingBox.lower.x = std::min(other.lower.x, _boundingBox.lower.x);
+    _boundingBox.lower.y = std::min(other.lower.y, _boundingBox.lower.y);
+    _boundingBox.lower.z = std::min(other.lower.z, _boundingBox.lower.z);
+
+    _boundingBox.upper.x = std::max(other.upper.x, _boundingBox.upper.x);
+    _boundingBox.upper.y = std::max(other.upper.y, _boundingBox.upper.y);
+    _boundingBox.upper.z = std::max(other.upper.z, _boundingBox.upper.z);
+}
+
+
+unsigned long long Node::getId() {
+    return _id;
+}
+
+// Utility functions to get all keys and all values from a map
+template <typename K, typename V>
+std::vector<K> BoundingVolumeHierarchy::getAllMapKeys(std::map<K, V> mp){
+
+    std::vector<K> keys;
+
+    for(auto const& it : mp) {
+        keys.push_back(it.first);
+    }
+
+    return keys;
+}
+
+template <typename K, typename V>
+std::vector<V> BoundingVolumeHierarchy::getAllMapValues(std::map<K, V> mp) {
+
+    std::vector<V> values;
+
+    for(const auto& it : mp) {
+        values.push_back(it->second);
+    }
+
+    return values;
+}
+
+template <typename K, typename V> 
+std::vector<V> BoundingVolumeHierarchy::getMapValuesOfKeys(std::map<K, V> mp, std::vector<K> keys) {
+    std::vector<V> values;
+
+    for(const auto& key : keys) {
+        values.push_back(mp[key]);
+    }
+
+    return values;
 }
--- src/bounding_volume_hierarchy.h
+++ src/bounding_volume_hierarchy.h
@@ -1,6 +1,106 @@
 #pragma once
 #include "ray_tracing.h"
 #include "scene.h"
+#include <map>
+#include <climits>
+#include <algorithm>
+#include <array>
+#include <exception>
+#include <utility>
+#include <queue>
+#include <iostream>
+
+// It would have made a lot more sense to have a BvhSphere class and a BvhTriangle class extend from BvhObject,
+// but unfortunetely, polymorphism cannot be achieved without pointers :(
+
+// Encapsulates an atomic object of Bounding Volume Hierarchy.
+// Can be either a triangle or a sphere, but has the potential to be extended to other shapes.
+
+class BvhObject
+{
+
+    static unsigned long long id;
+
+public:
+    BvhObject();
+
+    // constructs an object from a triangle
+    BvhObject(glm::vec3 &v0, glm::vec3 &v1, glm::vec3 &v2);
+
+    // constructs an object of a sphere
+    BvhObject(Sphere &sphere);
+
+    // returns the AABB corresponding to the object
+    AxisAlignedBox &getAABB();
+
+    // returns a string with the object type, either 'triangle' or 'sphere'
+    std::string getType();
+
+    // returns the object's unique id
+    unsigned long long getId();
+
+    std::array<glm::vec3, 3> &getTriangle();
+    Sphere& getSphere();
+
+    BvhObject operator=(BvhObject other);
+
+    private :
+    // default constructor, called by all of the public constructor
+
+    // the type of the object, either triangle or sphere
+    std::string _type;
+
+    const std::string _SPHERE_TYPE = "sphere";
+    const std::string _TRIANGLE_TYPE = "triangle";
+
+    // unique id for the object.
+    unsigned long long _id;
+
+    AxisAlignedBox _boundingBox;
+
+    // use 'type' to distiguish between triangle and sphere
+    std::array<glm::vec3, 3> _triangle; // stores the triangle, in case this object wraps around a triangle
+    std::array<Sphere, 1> _sphere;      
+};
+
+struct Node
+{
+    static unsigned long long id;
+
+    // ctr
+    // takes a vector with all the BvhObjects in it's subtree. Used to construct the AABB
+    // isLeaf should be true if this is a leaf node, false otherwise.
+    Node(std::vector<BvhObject> &containingBvhObjectss, bool isLeaf);
+
+    Node();
+
+    // add child Node, only for non-leaves
+    void addChild(Node& other);
+
+    // add child BvhObject, only for leaves
+    void addChild(BvhObject &object);
+
+    // returns true if the node is a leaf, otherwise false
+    bool isLeaf();
+
+    // returns the id of the node
+    unsigned long long getId();
+
+    AxisAlignedBox _boundingBox;
+
+private:
+    void updateAABB(AxisAlignedBox& other);
+
+
+    bool _isLeaf;
+
+    std::vector<unsigned long long> _children; // contains the ids of the children of this node. These ids are either node ids, or bvhObject ids, in the case of leaf nodes
+
+    unsigned long long _id;
+};
+
+
+
 
 class BoundingVolumeHierarchy {
 public:
@@ -15,6 +115,44 @@ public:
     // is on the correct side of the origin (the new t >= 0).
     bool intersect(Ray& ray, HitInfo& hitInfo) const;
 
-private:
-    Scene* m_pScene;
-};
+    // loads the data from the scene as BvhObjects
+    void addBvhObjectsFromScene();
+
+    // bvhObjectIds - the ids of the objects that are port of this node's subtree
+    // level - the level of this node
+    // isLeaf - true if the node should be a leaf node, false otherwise
+    unsigned long long createNode(std::vector<unsigned long long> &bvhObjectIds, unsigned int level);
+
+    private :
+
+        // prints a textual representation of the hierarchy tree. Useful for debugging
+        void printHierarchy();
+
+        unsigned int _max_level = 8;
+
+        unsigned int _max_level_achieved = 0;
+
+        // the root node
+        unsigned _root_id;
+
+        // performs a split of the provided objects, based on the median value
+        // split_axis = 0 - for x_axis; 1 - for y_axis; 2 - for z_axis
+        std::array<std::vector<unsigned long long>, 2> medianSplit(std::vector<unsigned long long> bvhObjectIds, unsigned int split_axis);
+
+        Scene *m_pScene;
+
+        std::map<unsigned long long, Node> nodes;                                      // map storing all of the nodes in the BVH indexed by their id.
+        std::map<unsigned long long, BvhObject> bvhObjects;                            // map storing all BvhObjects indexed by their id
+        std::map<unsigned long long, std::vector<unsigned long long>> objects_at_node; // map storing the ids of all the objects corresponding to one Node
+        std::map<unsigned int, std::vector<unsigned long long>> nodes_at_level;        // stores the ids of all of the nodes located at a certain level
+
+        // Utility functions to get all keys and all values from a map
+        template <typename K, typename V>
+        std::vector<K> getAllMapKeys(std::map<K, V> mp);
+
+        template <typename K, typename V>
+        std::vector<V> getAllMapValues(std::map<K, V> mp);
+
+        template <typename K, typename V>
+        std::vector<V> getMapValuesOfKeys(std::map<K, V> mp, std::vector<K> keys);
+    };
